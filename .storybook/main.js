19b8fae6c17398d2e72c49de0481b6325abae89e
miltonejones <miltonejones@gmail.com>
2022-03-02 14:18:18 -0600

    removing unneeded folders

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,8 +7,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-    "./addons/register",
+    "@storybook/preset-create-react-app"
   ],
   "framework": "@storybook/react",
   "core": {
